c50352c5b820427aaf881ff87491167ac7665149
hamura48 <90378399+hamura48@users.noreply.github.com>
2022-01-03 00:26:35 +0600

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except TypeError:
-    UPSTREAM_REPO = "https://github.com/hamura48/MIRROR-HUNTER/tree/qbit"
+    UPSTREAM_REPO = "https://github.com/hamura48/MIRROR-HUNTER"
 
 if os.path.exists('.git'):
     subprocess.run(["rm", "-rf", ".git"])
